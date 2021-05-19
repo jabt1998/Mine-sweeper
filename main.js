5ebea19b961379336d92ae8a1b41182741cdaec5
James Thomas <jabt1998@gmail.com>
2021-05-19 10:40:23 +0100

    added files

---
+++ main.js
@@ -0,0 +1,16 @@
+console.log('hello')
+
+
+
+let numArr = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+
+const generateField = (v) => {
+   for ( i = 0; i <= v.length-1; i++) {
+    if ( i === 0 ) {
+      document.write('<button></button')
+    }
+  }
+}
+
+generateField(numArr)
